67f2172a1c88b5f6729fd20ccaa6a55a5399381b
daria <147499446+DariaSokolova12@users.noreply.github.com>
2024-03-14 12:11:14 +0300

    ver1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,85 @@
-// @todo: Темплейт карточки
+import './pages/index.css';
+import { initialCards } from './cards.js';
+// @todo: Темплейт карточки 
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content; 
 
-// @todo: Функция создания карточки
+ 
 
-// @todo: Функция удаления карточки
+// @todo: DOM узлы 
 
-// @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list'); 
+
+ 
+
+// @todo: Функция создания карточки 
+
+function createCard(cardData, deleteCardCallback) { 
+
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true); 
+
+ 
+
+  const cardImage = cardElement.querySelector('.card__image'); 
+
+  const cardTitle = cardElement.querySelector('.card__title'); 
+
+ 
+
+  cardImage.src = cardData.link; 
+
+  cardImage.alt = cardData.name; 
+
+  cardTitle.textContent = cardData.name; 
+
+ 
+
+  const deleteIcon = cardElement.querySelector('.card__delete-button'); 
+
+  deleteIcon.addEventListener('click', function () { 
+
+    deleteCardCallback(cardElement); 
+
+  }); 
+
+ 
+
+  return cardElement; 
+
+} 
+
+ 
+
+// @todo: Функция удаления карточки 
+
+function deleteCard(cardElement) { 
+
+  if (cardElement) { 
+
+    cardElement.remove(); 
+
+  } 
+
+} 
+
+ 
+
+// @todo: Вывести карточки на страницу 
+
+function renderInitialCards() { 
+
+  initialCards.forEach((cardData) => { 
+
+    const cardElement = createCard(cardData, deleteCard); 
+
+    placesList.appendChild(cardElement); 
+
+  }); 
+
+} 
+
+ 
+
+// Вызываем функцию для вывода карточек на страницу 
+
+renderInitialCards();
