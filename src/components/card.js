f3a90e87655000998f3e9c195559f8c69bef2af0
Daria Sokolova <dariasokolova@192.168.1.3>
2024-10-03 13:36:37 +0300

    code design

--- src/components/card.js
+++ src/components/card.js
@@ -1,57 +1,65 @@
-// Создание нового карточного элемента
-const cardTemplate = document.querySelector('#card-template').content; 
+import { openModal } from "./modal";
 
+const createCard = (cardData) => {
+  // Создание нового карточного элемента
+  const cardTemplate = document.querySelector("#card-template").content;
+  // Клонируем шаблон карточного элемента
+  const cardElement = cardTemplate.cloneNode(true).querySelector(".card");
+  // Получаем элементы карточного элемента
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  // Устанавливаем значения карточного элемента
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+  // Добавляем обработчик удаления карточного элемента
+  const deleteIcon = cardElement.querySelector(".card__delete-button");
+  deleteIcon.addEventListener("click", () => {
+    deleteCard(cardElement);
+  });
+  // Добавляем обработчик нажатия на кнопку "Like"
+  const likeButton = cardElement.querySelector(".card__like-button");
+  likeButton.addEventListener("click", (event) => likeCard(event));
+  // Добавляем обработчик нажатия на картинку
+  cardImage.addEventListener("click", () => clickHandleImage(cardData));
+  
+  return cardElement;
+}
 
-function createCard(cardData, deleteCardCallback, imageOpenCallback) { 
-
-    // Клонируем шаблон карточного элемента
-    const cardElement = cardTemplate.cloneNode(true).querySelector('.card'); 
-  
-    // Получаем элементы карточного элемента
-    const cardImage = cardElement.querySelector('.card__image'); 
-    const cardTitle = cardElement.querySelector('.card__title'); 
-  
-    // Устанавливаем значения карточного элемента
-    cardImage.src = cardData.link; 
-    cardImage.alt = cardData.name; 
-    cardTitle.textContent = cardData.name; 
-  
-    // Добавляем обработчик удаления карточного элемента
-    const deleteIcon = cardElement.querySelector('.card__delete-button'); 
-    deleteIcon.addEventListener('click', function () { 
-      deleteCardCallback(cardElement); 
-    }); 
-
-    // Добавляем обработчик нажатия на кнопку "Like"
-    const likeButton = cardElement.querySelector('.card__like-button');
-    likeButton.addEventListener('click', likeCard);
-
-    // Добавляем обработчик нажатия на картинку
-    cardImage.addEventListener('click', () => { 
-        imageOpenCallback(cardData);
-    }); 
-        
-  
-    return cardElement; 
-  
-} 
+/**
+ * Обработчик клика по изображению
+ */
+const clickHandleImage = (cardData) => {
+  const previewImage = document.querySelector(".popup__image");
+  const previewImageModal = document.querySelector(".popup_type_image");
+  const captionModal = document.querySelector(".popup__caption");
+  // Проверяем, что элементы модального окна существуют
+  if (previewImage && previewImageModal && captionModal) {
+    previewImage.src = cardData.link;
+    previewImage.alt = cardData.name;
+    captionModal.textContent = cardData.name;
+    openModal(previewImageModal); // Открытие модального окна с изображением
+  } else {
+    console.error(
+      "Не удалось найти элементы модального окна просмотра изображения",
+    );
+  }
+};
 
 /**
  * Удаление карточного элемента
  */
-function deleteCard(cardElement) { 
-    if (cardElement) { 
-      cardElement.remove(); 
-    } 
-}
+const deleteCard = (cardElement) => {
+  if (cardElement) cardElement.remove();
+};
 
 /**
  * Обработчик нажатия на кнопку "Like"
  */
 const likeCard = (evt) => {
-    const cardLikeButton = evt.target;
-    cardLikeButton.classList.toggle("card__like-button_is-active");
+  const cardLikeButton = evt.target;
+  cardLikeButton.classList.toggle("card__like-button_is-active");
 };
 
 // Экспортируем функции для использования в других файлах
-export { createCard, deleteCard, likeCard };
+export { createCard };
--- src/components/index.js
+++ src/components/index.js
@@ -1,65 +1,32 @@
 import "../index.css";
 import { initialCards } from "./cards";
-import { createCard, deleteCard, likeCard } from "./card";
+import { createCard } from "./card";
 import { openModal, closeModal } from "./modal";
 
-// DOM узлы 
-
-const placesList = document.querySelector('.places__list'); 
-
+// DOM узлы
+const placesList = document.querySelector(".places__list");
 const editButton = document.querySelector(".profile__edit-button");
-
 const profileTitle = document.querySelector(".profile__title");
-
 const profileJob = document.querySelector(".profile__description");
-
 const addButton = document.querySelector(".profile__add-button");
-
 const modalEditProfile = document.querySelector(".popup_type_edit");
-
 const editProfileForm = modalEditProfile.querySelector(".popup__form");
-
 const nameInput = editProfileForm.querySelector('input[name="name"]');
-
 const jobInput = editProfileForm.querySelector('input[name="description"]');
-
 const modalAddCard = document.querySelector(".popup_type_new-card");
-
 const addForm = modalAddCard.querySelector(".popup__form");
-
 const cardNameInput = addForm.querySelector('input[name="place-name"]');
-
 const cardLinkInput = addForm.querySelector('input[name="link"]');
-
-const previewImage = document.querySelector('.popup__image');
-const previewImageModal = document.querySelector('.popup_type_image');
-const captionModal = document.querySelector('.popup__caption');
-
-/**
- * Обработчик клика по изображению
- */
-const clickHandleImage = (cardData) => {
-  // Проверяем, что элементы модального окна существуют
-  if (previewImage && previewImageModal && captionModal) {
-    previewImage.src = cardData.link;
-    previewImage.alt = cardData.name;
-    captionModal.textContent = cardData.name;
-    openModal(previewImageModal);  // Открытие модального окна с изображением
-  } else {
-    console.error('Не удалось найти элементы модального окна просмотра изображения');
-  }
-};
 
 /**
  * Отрисовывает инициализирующие карточные элементы
  */
-function renderInitialCards() { 
-  initialCards.forEach((cardData) => { 
-    const cardElement = createCard(cardData, deleteCard, clickHandleImage); 
-    placesList.appendChild(cardElement); 
-  }); 
-}
-
+const renderInitialCards = () => {
+  initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData);
+    placesList.appendChild(cardElement);
+  });
+};
 // Инициализация карточек при загрузке страницы
 renderInitialCards();
 
@@ -101,15 +68,11 @@ const submitAddForm = (evt) => {
   evt.preventDefault();
   const cardData = {
     name: cardNameInput.value,
-    link: cardLinkInput.value
+    link: cardLinkInput.value,
   };
-  
-  const addCard = createCard(
-    cardData,
-    deleteCard,
-    clickHandleImage  
-  );
-  
+
+  const addCard = createCard(cardData);
+
   placesList.prepend(addCard);
   addForm.reset();
   closeModal(modalAddCard);
