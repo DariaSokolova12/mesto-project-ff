fac562c1389afb1f96a76e69823e6d4414edd663
Daria Sokolova <dariasokolova@192.168.1.4>
2024-11-18 19:10:30 +0200

    исправление классов

--- src/components/validation.js
+++ src/components/validation.js
@@ -1,11 +1,4 @@
-enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-});
+
 
 // Функция, которая добавляет класс с ошибкой
 const showInputError = (formElement, inputElement, errorMessage) => {
